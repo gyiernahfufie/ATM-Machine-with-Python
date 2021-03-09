149c468dc1101896011dc0ea52a9fb36ceba858f
gyiernahfufie <65986450+gyiernahfufie@users.noreply.github.com>
2021-03-09 11:00:35 +0800

    Add files via upload

---
+++ ATM MACHINE ADMIN INTERFACE.py
@@ -0,0 +1,83 @@
+#HOO YEN HUI
+#TP059474
+
+import string
+import random
+
+cust_profile = open("cust_profile.txt","a+")
+
+cnt = 1
+temp =()
+address = ()
+
+def randompassword():
+  chars = string.ascii_uppercase + string.ascii_lowercase + string.digits
+  size = random.randint(8, 12)
+  return ''.join(random.choice(chars) for x in range(size))
+
+print('Hello! Welcome to APU Bank.')
+print('This is Admin account.')
+
+
+choice = int(input('Enter 0 to proceed, 1 to terminate:'))
+
+while True:
+    if(choice == 0):
+      print('What would you like to do today? \n1.Create Customer profile\n2. View customer transaction history\n3. Search customer profile\n')
+      choice = int(input('Enter your choice:'))
+      if (choice == 1):
+        firstname = input('Enter First Name:')
+        lastname = input('Enter Last Name:')
+        address = input('Enter Address:')
+        username = lastname[0:2] + firstname
+        username_integer = username + str(cnt)
+        with open ('cust_profile.txt','r') as f:
+          data = f.read()
+          if (username not in data):
+              temp = username
+              temp2 = randompassword()
+              cust_profile.write('Username:'+username+'\t'+'Password:'+temp2+'\t'+'First Name:'+firstname+'\t'+'Last Name:'+ lastname+'\t'+'Address:'+ address+"\n")
+              print("\nCreated username is:",temp,'and password is',temp2,'\n')
+
+          else:
+              cnt = cnt + 1
+              temp = username_integer
+              temp2 = randompassword()
+              cust_profile.write('Username:'+username_integer+'\t'+'Password:'+temp2+'First Name:'+firstname+'\t'+'Last Name:'+ lastname+'\t'+'Address:'+ address+"\n")
+              print('\nCreated username is:',temp,'and password is',temp2,"\n")
+            
+      elif(choice == 2):
+        username = input('Enter username that you would like to view:')
+        transactionfilename = username +'_transactionhistory.txt'
+        transactionfile = open(transactionfilename,'a')
+        with open (transactionfilename,'r') as f:
+          f.seek(0)
+          if f.read(1):
+            print(f.read())
+            break
+          else:
+            print('There is no transaction history for this customer')
+            break
+      elif(choice == 3):
+        data = input('Please enter customer detail to search:')
+        file = open ('cust_profile.txt')
+        for line in file:
+          line = line.rstrip()
+          if data in line:
+            print(line)
+        break
+
+      else:
+        print('Wrong Option')
+        choice = int(input('Enter 0 to proceed, 1 to terminate:'))
+
+    elif (choice == 1):
+        print('Goodbye')
+        break
+    else:
+        print('Wrong option')
+        choice = int(input('Enter 0 to proceed, 1 to terminate:'))
+
+cust_profile.close()
+
+
---
+++ ATM MACHINE CUSTOMER INTERFACE.py
@@ -0,0 +1,123 @@
+#HOO YEN HUI
+#TP059474
+
+import os
+import datetime
+
+def depositcash():
+    deposit_amount = float(input('Please enter amount to be deposited:'))
+    username = input('Please enter username:')
+    filename = username+'.txt'
+    transactionfilename = username+'_transactionhistory.txt'
+    transactionfile = open(transactionfilename,'a')
+    file = open(filename,'a')
+    balance = 0
+    new_balance = 0
+    with open(filename,'r') as file:
+        for i in file:
+            balance = float(i)
+    with open(filename,'w+') as file2:
+        new_balance = balance + deposit_amount
+        new_balance = str(new_balance)
+        file2.write(new_balance)
+        print('Your new balance is:',new_balance)
+    with open (transactionfilename,'a') as file3:
+        balance = str(balance)
+        deposit_amount = str(deposit_amount)
+        file3.write(str(datetime.datetime.now()))
+        file3.write('\nPrevious balance: '+balance+'. Deposit amount is:'+deposit_amount+'. New balance:'+new_balance+'\n')
+    
+    file.close()
+
+def withdrawalcash():
+    withdrawn_amount = float(input('Please enter amount to be withdrawn:'))
+    username = input('Please enter username:')
+    filename = username+'.txt'
+    transactionfilename = username+'_transactionhistory.txt'
+    transactionfile = open(transactionfilename,'a')
+    file = open(filename,'a')
+    balance = 0
+    new_balance = 0
+
+    with open(filename,'r') as file:
+        for i in file:
+            balance = float(i)
+    with open(filename,'w+') as file2:
+        if withdrawn_amount > balance:
+            print('You have insufficient balance. Your balance is:',balance)
+            
+            
+        else:
+            new_balance =  balance - withdrawn_amount
+            new_balance = str(new_balance)
+            file2.write(new_balance)
+            print('Your new balance is:',new_balance)
+            with open (transactionfilename,'a') as file3:
+                balance = str(balance)
+                withdrawn_amount = str(withdrawn_amount)
+                file3.write(str(datetime.datetime.now()))
+                file3.write('\nPrevious balance: '+balance+'. Withdrawn amount is:'+withdrawn_amount+'. New balance:'+new_balance+'\n')
+    file.close()
+
+def balance():
+    username = input('Please enter username:')
+    filename = username+'.txt'
+    file = open(filename,'a')
+    with open(filename,'r') as file:
+        for i in file:
+            balance = float(i)
+        file.seek(0)
+        if file.read(1):
+            print ('Your balance is:', balance)
+            
+        else:
+            print('You have no balance left.')  
+    file.close()
+    
+print('Hello ! Welcome to APU bank !')
+print('Please login to proceed.')
+
+cust_profile = open('cust_profile.txt','r')
+data = cust_profile.read()
+username = input('Please enter your username:')
+password = input('Please enter your password:')
+if username in data and password in data:
+    print('Login Successful')
+    opt = int(input('Do you want to continue? Enter 0 to continue, 1 to Terminate:'))
+    while True:
+        if (opt == 0):
+            print('What would you like to do today? \n1. Deposit Money\n2. Withdraw Money\n3. Check Balance\n4. Check Transaction History')
+            opt = int(input('Enter your choice:'))
+            if(opt == 1):
+                depositcash()
+                break
+            elif(opt == 2):
+                 withdrawalcash()
+                 break
+            elif(opt == 3):
+                 balance()
+                 break
+            elif(opt == 4):
+                transactionfilename = username+'_transactionhistory.txt'
+                transactionfile = open(transactionfilename,'a')
+                with open (transactionfilename,'r') as f:
+                    f.seek(0)
+                    if f.read(1):
+                        print(f.read())
+                        break
+                    else:
+                        print('There is no transaction history.')
+                        break
+
+                file.close()
+            else:
+                print('Wrong Option')
+                opt= int(input('Do you want to continue? Select 0 to Continue, 1 to Terminate:'))
+        elif (opt == 1):
+            print('Goodbye')
+            break
+        else:
+            print('Wrong option')
+            opt= int(input('Do you want to continue? Select 0 to Continue, 1 to Terminate.'))
+else:
+    print('Incorrect Username or Password. Please try again later or contact our Customer Care Representative.')
